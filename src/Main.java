584dadc9c375c6aa5b3354683647eb9e7a636198
Long Nguyen <longnguyen11898@gmail.com>
2024-03-31 17:44:59 +0700

    format code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,30 +10,30 @@ public class Main {
         do {
             System.out.println("Enter a size: ");
             size = scanner.nextInt();
-            if (size > 20){
+            if (size > 20) {
                 System.out.println("Size shold not exceed 20");
             }
         } while (size > 20);
 
         array = new int[size];
         int i = 0;
-        while (i <array.length){
+        while (i < array.length) {
             System.out.println("Enter element " + (i + 1) + " : ");
             array[i] = scanner.nextInt();
             i++;
         }
 
         System.out.println("Property list: ");
-        for (int j = 0; j < array.length; j++){
+        for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
 
-        int max =array[0];
+        int max = array[0];
         int index = 1;
-        for (int j =0; j < array.length; j++) {
+        for (int j = 0; j < array.length; j++) {
             if (array[j] > max) {
                 max = array[j];
-                index = j +1;
+                index = j + 1;
             }
         }
         System.out.println("The largest property value in the list is " + max + ", at position " + index);
