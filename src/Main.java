75cd180473ced856e43c123ccb88bb5e842c2912
Long Nguyen <longnguyen11898@gmail.com>
2024-03-31 12:33:34 +0700

    Finish Day 4 Practice 3

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size;
+        int[] array;
+
+        do {
+            System.out.println("Enter a size: ");
+            size = scanner.nextInt();
+            if (size > 20){
+                System.out.println("Size shold not exceed 20");
+            }
+        } while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i <array.length){
+            System.out.println("Enter element " + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        System.out.println("Property list: ");
+        for (int j = 0; j < array.length; j++){
+            System.out.print(array[j] + "\t");
+        }
+
+        int max =array[0];
+        int index = 1;
+        for (int j =0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j +1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + ", at position " + index);
+
+    }
+}
